6d9978f9ae1490fa980f3c44229b8e2a5ab3e148
mauro40ar <mauro39ar@gmail.com>
2020-06-24 19:29:51 +0000

    Practicando

---
+++ programa1poo.py
@@ -0,0 +1,7 @@
+class Persona():
+    def __init__(self,nombre):
+        self.nombre=nombre
+    def imprimir(self):
+        print (self.nombre)
+p1=Persona(input('ingrese nombre completo: '))
+p1.imprimir()
